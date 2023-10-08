1daf9a3395e9dc908c5df6c6977364b36e96a507
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2023-10-08 21:20:05 +0200

    update 2023.10.08

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getAvailableBooks (books) {
+/* function getAvailableBooks (books) {
  let availableBookTitles = ""
  for ( const book of books) {
     if (book.stock > 0) {
@@ -19,4 +19,47 @@ const books = [
 
   const availableBookMessage = getAvailableBooks (books);
 
-  console.log(availableBookMessage);
+  console.log(availableBookMessage); */
+
+
+/*   // -------------------------- arrow function
+  let addTwoNumbers = (a,b) => {
+    return a + b
+  }
+
+  let result = addTwoNumbers(3,5)
+  let otherResult = addTwoNumbers(8,9)
+  console.log (result)
+  console.log (otherResult)
+
+// egy sorosoknal elhagyhato a kapcsos zarojel
+  let otherAdd = (a,b) => a + b;
+  let rresult = otherAdd(8,8)
+  console.log(rresult)
+
+  // ha egy paraméter van, a zárójelen elhagyhatoak
+  let greet = name =>`Hello ${name}`
+  console.log(greet("David")); */
+
+
+  // Callback
+/*   let num = 8;
+  function multiply (theNumber) {
+    return theNumber * 2;
+  }
+  multiply(num);
+  console.log(multiply(num)) */
+
+/*   function theFunction (name, callback) {
+    console.log(`Hello ${name}!`);
+    callback();
+  }
+
+  function bye() {
+    console.log("ByeBye!")
+  }
+
+  theFunction("David", bye) */
+
+  
+  // Builtin Features
